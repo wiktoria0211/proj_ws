7b72509099dd2326e4cb599d2432b9a1cd03720d
wiktoria0211 <149382166+wiktoria0211@users.noreply.github.com>
2024-06-20 22:59:16 +0200

    homejs
    work in progess

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,3 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./ Home.css";
+import szkola from "./szkola.jpg";
+
+function Home({ text }) {
+  return (
+    <div className="home">
+      <div className="home_bottom">
+        <div className="home_left">
+          <img className="szkola" src={szkola} alt="logo" />
+        </div>
+
+        <div className="home_right">
+          <div className="home_right_title">GEOPORTAL</div>
+          <div className="home_right_subtitle">
+            Projekt systemu do zarządzania szkołami w mieście i uczniami
+            przypisanymi do danej szkoły oraz nauczycielami przypisanymi do
+            danej klasy.
+          </div>
+          <Link to="services">
+            <button className="home_right_button">ROZPOCZNIJ</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
